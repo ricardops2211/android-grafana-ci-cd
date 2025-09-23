e9c4d85b6b594a9e70596739e7c66f740e9a1a80
Ricardo Pachas Sifuentes <ricardops2211@gmail.com>
2025-09-22 19:46:58 -0500

    change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
-  id("com.android.application") version "8.5.2"
-  kotlin("android") version "2.0.0" // si usas Kotlin; ajusta a tu versión
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
 }
 
 android {
   namespace = "com.example.app"
   compileSdk = 34
-
   defaultConfig {
     applicationId = "com.example.app"
     minSdk = 24
@@ -15,7 +14,6 @@ android {
     versionName = "1.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
-
   buildTypes {
     release {
       isMinifyEnabled = false
@@ -31,7 +29,6 @@ dependencies {
   implementation("androidx.core:core-ktx:1.13.1")
   implementation("androidx.appcompat:appcompat:1.7.0")
   implementation("com.google.android.material:material:1.12.0")
-
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.2.1")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,10 @@ pluginManagement {
     mavenCentral()
     gradlePluginPortal()
   }
+  plugins {
+    id("com.android.application") version "8.5.2"
+    id("org.jetbrains.kotlin.android") version "1.9.24"  // 👈 unificamos aquí
+  }
 }
 
 dependencyResolutionManagement {
