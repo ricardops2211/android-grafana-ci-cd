f437920c10432377b9cde86335ac6f603c3b7a0a
Ricardo Pachas Sifuentes <ricardops2211@gmail.com>
2025-09-22 23:03:43 -0500

    change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("com.android.application") version "8.5.2"
-  id("org.jetbrains.kotlin.android") version "1.9.24"
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -11,10 +11,14 @@ android {
     applicationId = "com.example.app"
     minSdk = 24
     targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+  kotlinOptions { jvmTarget = "17" }
 
   buildTypes {
     release {
@@ -25,29 +29,13 @@ android {
       )
     }
   }
-
-  // Java/Kotlin 17
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-  }
-  kotlinOptions {
-    jvmTarget = "17"
-  }
 }
 
 dependencies {
   implementation("androidx.core:core-ktx:1.13.1")
   implementation("androidx.appcompat:appcompat:1.7.0")
-  implementation("com.google.android.material:material:1.12.0")
-  implementation("androidx.activity:activity-ktx:1.9.2")
-  implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
+  implementation("com.google.android.material:material:1.12.0") // Material 3
   testImplementation("junit:junit:4.13.2")
-
-  // UI/instrumentation
-  androidTestImplementation("androidx.test:core:1.5.0")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation("androidx.test:rules:1.5.0")
 }
